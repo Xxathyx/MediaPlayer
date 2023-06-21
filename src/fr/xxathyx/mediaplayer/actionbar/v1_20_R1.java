390ca1751dbea9a8ad9099b44f68f17a08dc9793
Xxathyx <50303043+Xxathyx@users.noreply.github.com>
2023-06-21 11:10:49 +0200

    #23

--- src/fr/xxathyx/mediaplayer/actionbar/v1_20_R1.java
+++ src/fr/xxathyx/mediaplayer/actionbar/v1_20_R1.java
@@ -1,7 +1,5 @@
 package fr.xxathyx.mediaplayer.actionbar;
 
-import java.lang.reflect.Field;
-
 import org.bukkit.entity.Player;
 
 import fr.xxathyx.mediaplayer.util.ActionBar;
@@ -19,14 +17,10 @@ import net.minecraft.server.network.PlayerConnection;
 public class v1_20_R1 implements ActionBar {
 		
 	@Override
-    public void send(Player player, String text) {
-		
-		Field[] fields = ((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle().getClass().getFields();
-		net.minecraft.server.network.PlayerConnection playerConnection;
-		
+    public void send(Player player, String text) {		
 		try {
-			playerConnection = (PlayerConnection) fields[0].get(((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle());
-			playerConnection.a(new net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket(
+			((PlayerConnection) ((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle().getClass().getFields()[0].get(
+					((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle())).a(new net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket(
 					net.minecraft.network.chat.IChatBaseComponent.ChatSerializer.a( "{\"text\": \"" + text + "\"}")));
 		}catch (IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
--- src/fr/xxathyx/mediaplayer/map/util/v1_20_R1.java
+++ src/fr/xxathyx/mediaplayer/map/util/v1_20_R1.java
@@ -1,6 +1,5 @@
 package fr.xxathyx.mediaplayer.map.util;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
@@ -23,14 +22,11 @@ public class v1_20_R1 implements MapUtil {
 	
 	@Override
 	public void update(Player player, int id, byte[] buffer) {
-				
-		Field[] fields = ((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle().getClass().getFields();
-		net.minecraft.server.network.PlayerConnection playerConnection;
-		
 		try {
-			playerConnection = (PlayerConnection) fields[0].get(((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle());
-	    	playerConnection.a(new net.minecraft.network.protocol.game.PacketPlayOutMap(id, (byte) 4, false,
-	    			new ArrayList<>(), new net.minecraft.world.level.saveddata.maps.WorldMap.b(0, 0, 128, 128, buffer)));
+			((PlayerConnection) ((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle().getClass().getFields()[0].get(
+					((org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer)player).getHandle())).a(
+							new net.minecraft.network.protocol.game.PacketPlayOutMap(id, (byte) 4, false, new ArrayList<>(),
+									new net.minecraft.world.level.saveddata.maps.WorldMap.b(0, 0, 128, 128, buffer)));
 		}catch (IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
